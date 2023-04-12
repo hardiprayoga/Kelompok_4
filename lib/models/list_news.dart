8b277f10c8e18a3be510e1c8a46697057e6e03de
Asri Nurjati <2006028@itg.ac.id>
2023-04-12 23:11:22 +0700

    menambahkan list news

---
+++ lib/models/list_news.dart
@@ -0,0 +1,27 @@
+import './news.dart';
+
+List<News> litslatesNews = [
+  News(
+    id: 'News 1',
+    imageUrl:
+        'https://i.postimg.cc/NFBxBshP/059557800-1626167508-sergio-capuzzimati-SITw-DBhar6w-unsplash.jpg',
+    title: '10 Jenis Ikan Koi Paling Populer dan Keunikannya',
+    content:
+        'Salah satu hal yang paling menarik tentang ikan koi adalah keragaman jenis dan warna yang dapat ditemukan di antara mereka. Dari Kohaku putih-merah klasik hingga kemilau metalik Kin Gin Rin, ada banyak jenis ikan koi yang bisa dipilih. Setiap jenis ikan koi memiliki karakteristik uniknya sendiri, seperti warna, pola, dan jenis skala, yang membuatnya menonjol dan menarik bagi selera yang berbeda. Ikan koi adalah spesies ikan hias yang menarik dan dicintai yang telah memikat hati banyak pemilik hewan peliharaan di seluruh dunia. Dengan warna cerah, pola khas, dan umur panjang, koi telah menjadi simbol keberuntungan, kemakmuran, dan ketekunan di banyak budaya. Setiap jenis ikan koi memiliki keunikan, yang menjadikannya subjek yang menarik untuk dipelihara atau sekadar mengagumi kecantikan ikan ini.',
+  ),
+  News(
+    id: 'News 2',
+    imageUrl: 'https://i.postimg.cc/qvh2fhhv/088737400-1664962686-page.jpg',
+    title: 'Pemilik Syok, Ikan Koi Seharga Ratusan Juta Dimakan Berang-Berang',
+    content:
+        'Salah satu hal yang paling menarik tentang ikan koi adalah keragaman jenis dan warna yang dapat ditemukan di antara mereka. Dari Kohaku putih-merah klasik hingga kemilau metalik Kin Gin Rin, ada banyak jenis ikan koi yang bisa dipilih. Setiap jenis ikan koi memiliki karakteristik uniknya sendiri, seperti warna, pola, dan jenis skala, yang membuatnya menonjol dan menarik bagi selera yang berbeda. Ikan koi adalah spesies ikan hias yang menarik dan dicintai yang telah memikat hati banyak pemilik hewan peliharaan di seluruh dunia. Dengan warna cerah, pola khas, dan umur panjang, koi telah menjadi simbol keberuntungan, kemakmuran, dan ketekunan di banyak budaya. Setiap jenis ikan koi memiliki keunikan, yang menjadikannya subjek yang menarik untuk dipelihara atau sekadar mengagumi kecantikan ikan ini.',
+  ),
+  News(
+    id: 'News 3',
+    imageUrl:
+        'https://i.postimg.cc/gJDgwXKP/001508600-1673423354-Screenshot-751.jpg',
+    title: 'BERANI BERUBAH: Dari Hobi, Kini Jadi Hoki dari Ikan Koi',
+    content:
+        'Sementara itu, Ahmad mematok harga ikan koi mulai Rp 150-250 ribu. Untuk ukurannya, itu beragam mulai 30-35 cm. Akan tetapi, kata Ahmad, perlu diingat bahwa ikan koi itu tidak diukur dari harganya yang mahal, namun dari kualitas ikan koi itu sendiri. “Ada yang size kecil itu udah Rp 1,5 juta, size besar malah cuma Rp 500 ribu itu ada,” tutur dia. Bisnis budi daya ikan koi Ahmad ini sudah menyebar dari Sabang sampai Merauke. Dia pernah mengirim ikan koi sampai ke Papua, Aceh, hingga Riau. Bahkan hingga sampai ke luar negeri, seperti ke Malaysia, Selangor, dan Kuala Lumpur. Dari situ Ahmad berhasil meraup keuntungan bukan hanya untuk diri sendir, tapi juga membuka peluang kerja untuk orang lain. Seperti salah satu pekerjanya bernama Ayuk Kurniawati. Dia merasa bersyukur bisa bekerja bersama Ahmad membudidayakan ikan koi ini. “Sebelum bekerja di sini, saya bekerja di salah satu restoran di Kota Semarang. Saya sangat bersyukur harapan saya ke depannya semoga semakin sukses, semakin dikenal oleh banyak orang, kemudian bisnisnya juga bisa berkembang lebih besar lagi bisa mempunyai greenhouse yang lebih besar lagi di Kota Semarang,” kata Ayuk. Sejak bisnis ini, Ahmad bersyukur bisa mendapatkan penghasilan untuk membeli sesuatu hingga menyenangkan keluarganya. Dia mengatakan, anak muda yang ingin mulai bisnis ikan koi harus punya semangat tinggi dan terus belajar dengan mentor yang tepat.',
+  ),
+];
